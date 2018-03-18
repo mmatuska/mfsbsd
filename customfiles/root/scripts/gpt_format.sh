a4896b753903e01e1405ea5fd06e8391c0e0bc1f
Joshua M. Clulow <jmc@joyent.com>
2018-03-18 17:45:40 +0000

    FREEBSD-3 gpt_format ignores errors (and has them) FREEBSD-11 gpt_format.sh usage message a trap for young players Reviewed by: Josh Wilsdon <josh@wilsdon.ca>

--- customfiles/root/scripts/gpt_format.sh
+++ customfiles/root/scripts/gpt_format.sh
@@ -4,12 +4,19 @@
 #
 # gpt_format.sh
 # 
-# example: gpt_format.sh -s 16G -d da0 da1 da2 da3\n"
+# example: gpt_format.sh -s 16G da0 da1 da2 da3\n"
 #
 
 usage() {
-	printf "Usage: $0 -s swap_size -d disk1 disk2...\n" 1>&2
-	printf "Example: $0 -s 16G -d da0 da1 da2 da3\n" 1>&2
+	printf "Usage: $0 -s swap_size disk1 disk2...\n" 1>&2
+	printf "Example: $0 -s 16G da0 da1 da2 da3\n" 1>&2
+}
+
+invalid() {
+	fmt="Invalid Input: $1\\n"
+	shift
+
+	printf "$fmt" "$@"
 }
 
 if [ $# -eq 0 ]; then
@@ -43,7 +50,7 @@ while getopts ":s:h" opt; do
 	esac
 done
 if [ -z ${SWAPSIZE} ]; then
-	printf "Invalid Input: Must set swapsize\n"
+	invalid 'Must set swapsize'
 	exit 2
 fi
 shift $(($OPTIND - 1))
@@ -51,9 +58,27 @@ shift $(($OPTIND - 1))
 # The rest of the arguments are devices
 DEVICES=${@}
 
+#
+# Before we do anything destructive, make sure GEOM is, on some level, aware
+# of each specified device.
+#
+for DEV in ${DEVICES}; do
+	if ! geom disk status ${DEV} >/dev/null; then
+		invalid 'device "%s" not found in "geom disk status"' ${DEV}
+		exit 3
+	fi
+done
+
 for DEV in ${DEVICES}; do
 	echo "------- Formatting ${DEV} -------"
-	gpart destroy -F ${DEV}
+
+	#
+	# Destroying the partition table will fail if this device does not yet
+	# have one.  This results in an unhelpful cosmetic wart ("Invalid
+	# argument"), so we throw the failure message out for now.
+	#
+	gpart destroy -F ${DEV} 2>/dev/null
+
 	gpart create -s gpt ${DEV}
 	gpart add -s ${SWAPSIZE} -t freebsd-swap -l swap ${DEV}
 	gpart add -t freebsd-zfs -l zpool ${DEV}
